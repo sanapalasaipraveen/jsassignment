91fdf8e34ba6a8450650c497686858c4b625d176
sanapalasaipraveen <saipraveensanapala776@gmail.com>
2022-01-24 16:29:34 +0530

    submitted createobject

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,11 @@
 function CreateObject(arr) {
     // Write your code here
+    var dict={}
+    for (var i=0;i<arr.length;i=i+2){
+        dict[arr[i]]=arr[i+1];
+    }
+    return dict;
+
 }
 
 module.exports = CreateObject;
